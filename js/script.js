9bedf186cc9cd9b717b206a924aa60464fe16cf3
Rashad Davis <rashaddavis@Rashads-MacBook-Pro.local>
2020-02-07 17:11:04 -0600

    switching branches

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*******CACHED RESOURCES********** */
+const squareEls = document.getElementsByClassName('piece');
+const start = document.getElementById('start');
+const round = document.getElementById('round');
+const message = document.getElementById('message');
+
+/*******EVENT LISTENERS*******/
