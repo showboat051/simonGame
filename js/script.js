29f03d4cf43c071158e4e9c49f2857c9ca56711e
Rashad Davis <rashaddavis@Rashads-MBP.attlocal.net>
2020-02-07 23:04:18 -0600

    working flash

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-/*******CACHED RESOURCES********** */
+/******* CACHED RESOURCES ********** */
 const squareEls = document.getElementsByClassName('piece');
 const start = document.getElementById('start');
 const round = document.getElementById('round');
 const message = document.getElementById('message');
 
-/*******EVENT LISTENERS*******/
+/******* EVENT LISTENERS *******/
+document.getElementById('playArea').addEventListener('click', handleButtonClick);
+document.getElementById('start').addEventListener('click', init);
+
+/******* FUNCTIONS *******/
+function init() {
+ handleStartGame(); 
+}
+
+function handleButtonClick() {
+}
+
+function handleStartGame () {
+  handleFlash();
+}
+function handleFlash() {
+  var f = document.getElementById('playArea');
+  setTimeout(function(){
+    f.style.filter = "brightness(50%)"; 
+  },200);
+  setInterval(function() {
+    f.style.filter = "brightness(100%)";
+  },500);
+}
