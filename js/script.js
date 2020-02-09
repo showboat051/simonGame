879f4a8abb8179c6ad5456117c2f2041efb12f74
Rashad Davis <rashaddavis@Rashads-MBP.attlocal.net>
2020-02-09 14:27:07 -0600

    getting random piece

--- js/script.js
+++ js/script.js
@@ -3,28 +3,50 @@ const squareEls = document.getElementsByClassName('piece');
 const start = document.getElementById('start');
 const round = document.getElementById('round');
 const message = document.getElementById('message');
+// const blue = document.getElementById('#blue');
+// const yellow = document.getElementById('#yellow');
+// const aqua = document.getElementById('#aqua');
+// const red = document.getElementById('red');
+// const green = document.getElementById('green');
+const square = ['#blue', '#yellow', '#aqua', '#red', '#green']
 
 /******* EVENT LISTENERS *******/
-document.getElementById('playArea').addEventListener('click', handleButtonClick);
+document.getElementById('playArea').addEventListener('click', event);
 document.getElementById('start').addEventListener('click', init);
 
 /******* FUNCTIONS *******/
 function init() {
- handleStartGame(); 
+  game: {
+    level: 1,
+    order = [],
+    playerOrder = [],
+    cpuOrder = []
+  }
+  cpuTurn();
 }
 
-function handleButtonClick() {
-}
-
-function handleStartGame () {
+function cpuTurn() {
+  // cpu will grab a random color
   handleFlash();
+  // flash random color
+  //  
 }
+
+function playerTurn() {
+  // player's response must match CPU
+  // Check if player matched CPU
+  // IF not call game over and restart 
+}
+
+  
 function handleFlash() {
-  var f = document.getElementById('playArea');
+  let randPiece = Math.floor(Math.random() * square.length); 
+  var s = document.getElementById('playArea');
   setTimeout(function(){
-    f.style.filter = "brightness(50%)"; 
+    s.style.filter = "brightness(50%)"; 
   },200);
   setInterval(function() {
-    f.style.filter = "brightness(100%)";
+    s.style.filter = "brightness(100%)";
   },500);
+  
 }
